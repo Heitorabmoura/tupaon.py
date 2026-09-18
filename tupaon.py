d9adc6dd1fbe205d4faa2b23c8719ea0d1940f77
Heitorabmoura <heitoralvesbmoura@gmail.com>
2026-09-18 18:27:27 -0300

    Meu primeiro commit

---
+++ tupaon.py
@@ -0,0 +1,11 @@
+nome = input("Qual é o seu nome? ")
+
+
+idade = int(input("Quantos anos você tem? "))
+
+
+print(f"Olá, {nome}! Você tem {idade} anos.")
+
+
+anos_para_100 = 100 - idade 
+print(f"Faltam {anos_para_100} anos para você completar 100 anos!")
